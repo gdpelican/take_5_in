d6d7119ae46b70ffe51b64223a7b1ae691fd0ecd
James Kiesel <james.kiesel@gmail.com>
2016-08-02 13:55:11 -0400

    Browserify transforms in package json

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,4 @@ lock '3.6.0'
 set :application, 'take_5_in'
 set :repo_url, 'https://github.com/gdpelican/take_5_in.git'
 set :user, 'take_five'
-set :npm_flags, '--silent --no-progress'    # default
+set :npm_flags, '--silent --no-progress'

9368c978eadc95b8ddc5cc971b04ea81b5b7b03d
James Kiesel <james.kiesel@gmail.com>
2016-08-05 23:56:53 -0400

    The start of header bg animations

--- src/components/app/header.jsx
+++ src/components/app/header.jsx
@@ -11,31 +11,48 @@ export default React.createClass({
   componentDidMount(data) {
     request.get(`${window.location.origin}/config`, (err, res, body) => {
       if (err) { console.log(err); return }
-      this.setState({config: JSON.parse(body), loading: false })
+      this.setState({config: JSON.parse(body) })
     })
   },
 
   render() {
-    if (this.state.loading) { return <Loading /> }
+    if (this.state.loading) {
+      return <header className="header">
+               <div className="header-background-image hidden" />
+               <Loading />
+               <img onLoad={this.imageLoaded} className="hidden" src={(this.selectedBackground() || {}).url} />
+             </header>
+    } else {
+      let config = this.state.config
+      let selected = this.selectedBackground()
+      let style = { backgroundImage: `url(${selected.url})` }
+      return <header className="header">
+               <div className="header-background-image" style={style} />
+               <img src={config.avatar} />
+               <div className="header-text">
+                 <p>{selected.title}</p>
+                 <p>{selected.subtitle}</p>
+               </div>
+               <div className="header-see-more">See More</div>
+               <BackgroundNavigator backgrounds={config.backgrounds} selected={this.state.selected} select={this.selectBackground} />
+             </header>
+    }
+  },
 
-    let config = this.state.config
-    let selected = this.selectedBackground()
-    let style = { backgroundImage: `url(${selected.url})` }
-    return <header className="header" style={style}>
-             <img src={config.avatar} />
-             <div className="header-text">
-               <p>{selected.title}</p>
-               <p>{selected.subtitle}</p>
-             </div>
-             <BackgroundNavigator backgrounds={config.backgrounds} selected={this.state.selected} select={this.selectBackground} />
-           </header>
+  imageLoaded() {
+    window.requestAnimationFrame(() => {
+      this.setState({ loading: false })
+    })
   },
 
   selectBackground(index) {
-    this.setState({ selected: index })
+    window.requestAnimationFrame(() => {
+      this.setState({ selected: index, loading: true })
+    })
   },
 
   selectedBackground() {
+    if (!this.state.config.backgrounds) { return null }
     return this.state.config.backgrounds.find((b) => { return b.index == this.state.selected })
   }
 })

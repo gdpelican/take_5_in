450849de914e5344d7b0b9fd2baa525cfe02edb5
James Kiesel <james.kiesel@gmail.com>
2016-09-10 21:39:14 +0100

    Use correct user

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,12 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, "#{ENV['TAKE_FIVE_USERNAME']}:#{ENV['TAKE_FIVE_PASSWORD']}@take-five.in"
+set :domain, "take-five.in"
 set :application, 'take_five_in'
 set :deploy_to, '/var/www/take_five_in'
 set :repository, 'https://github.com/gdpelican/take_5_in.git'
 set :branch, 'master'
+set :term_mode, nil
 
 # For system-wide RVM install.
 set :rvm_path, '/usr/local/rvm/scripts/rvm'
@@ -24,7 +25,7 @@ set :rvm_path, '/usr/local/rvm/scripts/rvm'
 set :shared_paths, ['log', 'tmp/pids', 'tmp/sockets']
 
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
+  set :user, 'take_five'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
 directory(ENV['RELEASE_DIRECTORY']) if ENV['RELEASE_DIRECTORY']
 
-preload_app!
-
 stdout_redirect 'log/puma.log', 'log/puma_error.log', true

c2bb978468254cf0348bcfbeba5312fd99346006
James Kiesel <james.kiesel@gmail.com>
2016-09-13 13:22:40 +0100

    Update sidebar copy

--- src/components/app/sidebar.jsx
+++ src/components/app/sidebar.jsx
@@ -8,9 +8,9 @@ export default React.createClass({
              <div className="sidebar-content">
                <h2>What is this?</h2>
                <p>
-                 Take Five In is a photo project, in which I take 5 pictures for every place that I go.
-                 Short, long, exciting, boring, tragic; doesn&#39;t matter, it&#39;s a small slice of life
-                 from someone living on the road, 100% of the time.
+                 Take Five In is an ongoing photo project, in which I take 5 pictures, accompanied by a short story,
+                 for every place that I go. Epic, surreal, tragic, mundane; doesn&#39;t matter,
+                 it&#39;s a small slice of life from someone living on the road, 100% of the time.
                </p>
                <hr />
                <h2>Who made it?</h2>
@@ -22,7 +22,7 @@ export default React.createClass({
                <h2>Want to hear more?</h2>
                <p>
                  Like something you see, want to hear more about a particular story or place, or want me to build you a website like this?
-                 Come say hi! You can reach me <a href="http://www.twitter.com/gdpelican">on twitter</a> or at <a href="mailto:james@take-five.in">james@take-five.in</a>
+                 Come say hi! You can reach me <a href="http://www.twitter.com/gdpelican">on twitter</a> or at <a href="mailto:james@take-five.in">james@take-five.in</a>.
                </p>
              </div>
            </BurgerMenu.scaleRotate>

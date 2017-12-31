f56e4267043fdef340ccc6fba40d7e04920b366a
James Kiesel <james.kiesel@gmail.com>
2017-12-31 20:24:48 +1300

    Spice up sidebar some

--- src/components/app/sidebar.jsx
+++ src/components/app/sidebar.jsx
@@ -6,20 +6,29 @@ export default React.createClass({
   render() {
     return <BurgerMenu.scaleRotate pageWrapId={"content"} outerContainerId={"app"}>
              <div className="sidebar-content">
-               <h2>What is this?</h2>
+               <div className="blurb-header">
+                 <img src="/dist/img/blurbs/takefive.png" />
+                 <h2>What is this?</h2>
+               </div>
                <p>
                  Take Five In is an ongoing photo project, in which I take 5 pictures, accompanied by a short story,
                  for every place that I go. Epic, surreal, tragic, mundane; doesn&#39;t matter,
                  it&#39;s a small slice of life from someone living on the road, 100% of the time.
                </p>
                <hr />
-               <h2>Who made it?</h2>
+               <div className="blurb-header">
+                 <img src="/dist/img/blurbs/james.png" />
+                 <h2>Who made it?</h2>
+               </div>
                <p>
                  My name&#39;s James; I&#39;m an open source software developer who&#39;s
                  currently travelling the world while hacking on human interaction with <a href="https://www.loomio.org">Loomio</a>.
                </p>
                <hr />
-               <h2>Want to hear more?</h2>
+               <div className="blurb-header">
+                 <img src="/dist/img/blurbs/wave.png" />
+                 <h2>Want to hear more?</h2>
+               </div>
                <p>
                  Like something you see, want to hear more about a particular story or place, or want me to build you a website like this?
                  Come say hi! You can reach me <a href="http://www.twitter.com/gdpelican">on twitter</a> or at <a href="mailto:james@take-five.in">james@take-five.in</a>.
